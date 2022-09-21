61d37926791dc9c19b30473a9c10cc465b0d19f5
RyuJaeJeong <ryoojj8998@gmail.com>
2022-09-21 23:46:41 +0900

    이진 검색 기반, 단순 삽입정렬 구현

--- src/sortingAlgorithm/InsertionSort.java
+++ src/sortingAlgorithm/InsertionSort.java
@@ -7,6 +7,18 @@ import java.util.Scanner;
  * 삽입정렬
  */
 public class InsertionSort {
+
+    /**
+     * 스왑 함수
+     * @param a
+     * @param idx1
+     * @param idx2
+     */
+    public static void swap(int[] a, int idx1, int idx2){
+        int temp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = temp;
+    }
     
     //단순삽입정렬
     public static void insertionSort(int[] a, int n){
@@ -19,6 +31,35 @@ public class InsertionSort {
             a[j] = tmp;
         }
     }
+
+    //단순삽입정렬
+    public static void insertionSort2(int[] a, int n){
+        for (int i = 1; i < n; i++) {
+            int j;
+            int tmp = a[i];
+            int idx;
+
+            if(i == 1){
+               if(a[0] > a[1]){
+                 swap(a, 0, 1);
+               }
+               continue;
+            }else{
+               idx = Arrays.binarySearch(Arrays.copyOf(a, i), tmp);
+            }
+
+            if(idx<0) {
+                idx = -idx;
+            }
+
+            for (j = i; j > idx - 1; j--) {
+                a[j] = a[j-1];
+            }
+
+            a[idx-1] = tmp;
+        }
+    }
+
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -32,7 +73,7 @@ public class InsertionSort {
             x[i] = sc.nextInt();
         }
 
-        insertionSort(x, nx);
+        insertionSort2(x, nx);
 
         System.out.println("오름차순으로 정렬 했습니다.!");
         System.out.println(Arrays.toString(x));
