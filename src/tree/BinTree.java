1bb5f98038b4ca9c9262afc6477595c63d7e7663
RyuJaeJeong <fb0982@naver.com>
2023-02-13 23:38:51 +0900

    TREE 연습

--- src/tree/BinTree.java
+++ src/tree/BinTree.java
@@ -37,6 +37,7 @@ public class BinTree<K, V> {
     private Node<K, V> root;
     private Comparator<? super K> comparator = null;
 
+
     //cons
     public BinTree() {  //comparator 없기때문에 자연수 순서로
         root = null;
@@ -85,7 +86,7 @@ public class BinTree<K, V> {
      * @param key
      * @param data
      */
-    private void add(K key, V data){
+    public void add(K key, V data){
         if(root == null)
             root = new Node<K,V>(key, data, null, null);
         else
@@ -114,4 +115,158 @@ public class BinTree<K, V> {
                 addNode(node.right, key, data);
         }
     }
+
+    /**
+     * 해당 Node 삭제
+     * @param key 삭제할 노드의 key값
+     * @return
+     */
+    public boolean remove(K key){
+        Node<K,V> p = root;
+        Node<K,V> parent = null;
+        boolean isLeftChild = true;
+
+        while(true){
+            if(p == null) return false;     //더이상 나아 갈 수 없다면
+            int condition = comp(key, p.getKey());
+            if(condition == 0){             //같으면 검색성공
+                break;
+            }else{
+                parent = p;                 //가지로 내려가기 전에 부모 설정
+                if(condition < 0){
+                    isLeftChild = true;
+                    p = p.left;
+                }else{
+                    isLeftChild = false;
+                    p = p.right;
+                }
+
+            }
+        }
+
+        if(p.left == null){
+            if(p == root){
+                root = p.right;
+            }else if(isLeftChild){
+                parent.left = p.right;
+            }else{
+                parent.right = p.right;
+            }
+        }else if(p.right == null){
+            if(p == root){
+                root = p.left;
+            }else if(isLeftChild){
+                parent.left = p.left;
+            }else{
+                parent.right = p.left;
+            }
+        }else{      //노드가 2개일때
+            parent = p;
+            Node<K,V> left = p.left;
+            isLeftChild = true;
+            while(left.right != null){
+                parent = left;
+                left = left.right;
+                isLeftChild = false;
+            }
+            p.key = left.key;
+            p.data = left.data;
+            if(isLeftChild){
+                parent.left = left.left;
+            }else{
+                parent.right = left.left;
+            }
+        }
+
+        return true;
+    }
+
+    private void printSubTree(Node node){
+        if(node != null){
+            printSubTree(node.left);
+            System.out.println(node.key + " " + node.data);
+            printSubTree(node.right);
+        }
+    }
+
+    public void print(){
+        printSubTree(root);
+    }
+
+    private void printSubTreeReverse(Node node){
+        if(node != null){
+            printSubTreeReverse(node.right);
+            System.out.println(node.key + " " + node.data);
+            printSubTreeReverse(node.left);
+
+        }
+    }
+
+    /**
+     * 모든 키 값의 내림차순으로 정렬 하는 메서드
+     */
+    public void printReverse(){
+        printSubTreeReverse(root);
+    }
+
+    /**
+     * @return 가장 작은 키값을 반환
+     */
+    public K getMinKey(){
+        Node<K,V> p = root;
+        if(p == null) return null;
+        while(true){
+            if(p.left == null){
+                return p.getKey();
+            }else {
+                p = p.left;
+            }
+        }
+    }
+
+    /**
+     * @return 가장 작은 키값을 갖는 데이터를 반환
+     */
+    public V getDataWithMinKey(){
+        Node<K,V> p = root;
+        if(p == null){
+            return null;
+        }else {
+            while(p.left != null){
+                p = p.left;
+            }
+            return p.getValue();
+        }
+    }
+
+    /**
+     * @return 가장 큰 키값을 반환
+     */
+    public K getMaxKey(){
+        Node<K,V> p = root;
+        if(p == null){
+            return null;
+        }else {
+            while(p.right != null){
+                p = p.right;
+            }
+            return p.getKey();
+        }
+    }
+
+    /**
+     * @return 가장 큰 키값을 갖는 데이터를 반환
+     */
+    public V getDataWithMaxKey(){
+        Node<K,V> p = root;
+        if(p == null){
+            return null;
+        }else {
+            while(p.right != null){
+                p = p.right;
+            }
+            return p.getValue();
+        }
+    }
+
 }
